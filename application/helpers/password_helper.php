9f46ce8adff4b0aa74be81e7f6efcd6b3e8adfb6
pcmagas <pc_magas@yahoo.gr>
2015-07-18 19:41:42 +0300

    Storing Password

--- application/helpers/password_helper.php
+++ application/helpers/password_helper.php
@@ -4,7 +4,7 @@ if(!function_exists('password'))
 	function password($password,$salt=null)
 	{
 		$return_salt=false;
-		if($return_salt=empty($salt)) $salt=md5(mcrypt_create_iv(22,MCRYPT_DEV_URANDOM));
+		if($return_salt=empty($salt)) $salt=substr(md5(mcrypt_create_iv(22,MCRYPT_DEV_URANDOM)),0,32);
 		
 		$opts=array('cost'=>12,'salt'=>$salt);
 		
