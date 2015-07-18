550df92538f820348378af8378d86d986add2d1b
pcmagas <pc_magas@yahoo.gr>
2015-07-18 17:49:13 +0300

    Md5 the salt

--- application/helpers/password_helper.php
+++ application/helpers/password_helper.php
@@ -4,7 +4,7 @@ if(!function_exists('password'))
 	function password($password,$salt=null)
 	{
 		$return_salt=false;
-		if($return_salt=empty($salt)) $salt=mcrypt_create_iv(22,MCRYPT_DEV_URANDOM);
+		if($return_salt=empty($salt)) $salt=md5(mcrypt_create_iv(22,MCRYPT_DEV_URANDOM));
 		
 		$opts=array('cost'=>12,'salt'=>$salt);
 		
