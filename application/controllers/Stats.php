a8e1c8fbcecec7ff180a4cabd56029197d5bd6a9
Dimitrios Desillas <ddesillas@yahoo.gr>
2015-10-24 13:40:41 +0300

    Add stats

--- application/controllers/Stats.php
+++ application/controllers/Stats.php
@@ -7,46 +7,46 @@ class Stats extends CI_Controller
 		parent::__construct();
 		$this->load->model('Stats_model','stats');
 	}
-	
+
 	function get()
 	{
 		$status='ok';
 		$message='';
-		
+
 		$data=$this->stats->get();
-		
+
 		if($data===-1)
 		{
-			$status='err';
-			$message='user_not_logedin';	
+			$status='log';
+			$message='user_not_logedin';
 		}
 		elseif($data===false)
 		{
 			$data=0;
 		}
-		
+
 		$this->load->view('json_view.php',array('status'=>$status,'data'=>$data,'message'=>$message));
 	}
-	
+
 	function set()
 	{
 		$status='ok';
 		$message='';
-		
+
 		$score=$this->input->post('score');
 		$data=$this->stats->set($score);
-		
+
 		if($data===-1)
 		{
-			$status='err';
-			$message='user_not_logedin';	
+			$status='log';
+			$message='user_not_logedin';
 		}
 		elseif($data===-2)
 		{
 			$status='err';
 			$message='invalid_credit';
 		}
-		
+
 		$this->load->view('json_view.php',array('status'=>$status,'data'=>$data,'message'=>$message));
 	}
 }
